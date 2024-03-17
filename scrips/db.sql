3077a4f71eecb9643c338cc7899cbc51838e294a
ArcangelGC <158417504+ArcangelGC@users.noreply.github.com>
2024-03-16 18:20:45 -0600

    Diagrama y Codifo
    Agruege atributos, entidades y relaciones.

---
+++ scrips/db.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE tiendadeabarrotes;
+USE tiendadeabarrotes;
+
+-- Crear la tabla Producto
+CREATE TABLE Producto (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(100),
+    Descripcion TEXT, O VARCHAR(n) 
+    Precio DECIMAL(10, 2),
+    Categoria VARCHAR(50),
+    Stock INT
+);
+
+-- Crear la tabla Cliente
+CREATE TABLE Cliente (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Telefono CHAR(13),
+    Direccion VARCHAR(255)
+);
+
+-- Crear la tabla Proveedor
+CREATE TABLE Proveedor (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(100),
+    Telefono CHAR(13),
+    Direccion VARCHAR(255)
+);
+
+-- Crear la tabla Empleado
+CREATE TABLE Empleado (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Puesto VARCHAR(50),
+    Salario DECIMAL(10, 2)
+);
+
+-- Creacion de la tabla Transaccion_Venta
+CREATE TABLE Transaccion_Venta (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Fecha DATE,
+    Cantidad INT,
+    Precio_total DECIMAL(10, 2),
+    Cliente_ID INT,
+    FOREIGN KEY (Cliente_ID) REFERENCES Cliente(ID)
+);
+
+-- Crear la tabla Transaccion_Producto (relación muchos a muchos entre Transaccion_Venta y Producto)
+CREATE TABLE Transaccion_Producto (
+    Transaccion_ID INT,
+    Producto_ID INT,
+    Cantidad INT,
+    PRIMARY KEY (Transaccion_ID, Producto_ID),
+    FOREIGN KEY (Transaccion_ID) REFERENCES Transaccion_Venta(ID),
+    FOREIGN KEY (Producto_ID) REFERENCES Producto(ID)
+);
